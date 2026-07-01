ce9c766d4b25b3a0f3c69552d40deb578ff64292
Elemen <3242388085@qq.com>
2026-07-01 21:36:30 +0800

    Update add event listener practice

--- problems/add-event-listener/code.js
+++ problems/add-event-listener/code.js
@@ -1,109 +1,58 @@
 class EventEmitter {
-    constructor() {
-        this.events = new Map();
+  constructor() {
+    this.events = new Map();
+  }
+
+  on(event, cb) {
+    if (!this.events.has(event)) {
+      this.events.set(event, new Set());
+    }
+    this.events.get(event).add(cb);
+  }
+
+  //一次订阅后，执行后就删除
+  once(event, cb) {
+    const wrapper = (...args) => {
+      this.off(event, wrapper);
+      cb(...args);
+    };
+    wrapper.origin = cb;
+    this.on(event, wrapper);
+  }
+
+  //取消订阅
+  off(event, cb) {
+    if (!this.events.has(event)) return;
+
+    const callbacks = this.events.get(event);
+
+    for (const fn of callbacks) {
+      if (fn === cb || fn.origin === cb) {
+        callbacks.delete(fn);
+      }
     }
 
-    addEventListener(event, callback) {
-        if (typeof callback !== 'function') {
-            throw new TypeError('callback must be a function');
-        }
-
-        if (!this.events.has(event)) {
-            this.events.set(event, new Set());
-        }
-
-        this.events.get(event).add(callback);
-        return this;
+    if (callbacks.size === 0) {
+      this.events.delete(event);
     }
+  }
 
-    on(event, callback) {
-        return this.addEventListener(event, callback);
+
+  //执行
+  emit(event, ...args) {
+    if (this.events.has(event)) {
+      const callbacks = this.events.get(event);
+
+      //复制一遍再删除，更稳定
+      for (const cb of [...callbacks]) {
+        cb(...args);
+      }
     }
-
-    once(event, callback) {
-        if (typeof callback !== 'function') {
-            throw new TypeError('callback must be a function');
-        }
-
-        const wrapper = (...args) => {
-            this.removeEventListener(event, wrapper);
-            callback(...args);
-        };
-
-        wrapper.origin = callback;
-        return this.addEventListener(event, wrapper);
-    }
-
-    removeEventListener(event, callback) {
-        const callbacks = this.events.get(event);
-
-        if (!callbacks) return this;
-
-        callbacks.forEach(cb => {
-            if (cb === callback || cb.origin === callback) {
-                callbacks.delete(cb);
-            }
-        });
-
-        if (callbacks.size === 0) {
-            this.events.delete(event);
-        }
-
-        return this;
-    }
-
-    off(event, callback) {
-        return this.removeEventListener(event, callback);
-    }
-
-    dispatchEvent(event, ...args) {
-        const callbacks = this.events.get(event);
-
-        if (!callbacks) return false;
-
-        [...callbacks].forEach(callback => callback(...args));
-        return true;
-    }
-
-    emit(event, ...args) {
-        return this.dispatchEvent(event, ...args);
-    }
-
-    removeAllListeners(event) {
-        if (event !== undefined) {
-            this.events.delete(event);
-        } else {
-            this.events.clear();
-        }
-
-        return this;
-    }
+  }
 }
 
-// 测试代码
 const emitter = new EventEmitter();
-
-function handleClick(data) {
-  console.log("click", data);
-}
-
-emitter.addEventListener("click", handleClick);
-
-emitter.dispatchEvent("click", {
-  name: "elemen"
-});// click { name: 'elemen' }
-
-
-emitter.removeEventListener(
-  "click",
-  handleClick
-);
-
-emitter.dispatchEvent("click");// 不会触发
-
-emitter.once("login", user => {
-  console.log("login once", user);
+emitter.once("click", (name) => {
+  console.log("点击发生了", name);
 });
-
-emitter.emit("login", "elemen");// login once elemen
-emitter.emit("login", "again");// 不会触发
+emitter.emit("click", "elemen");
